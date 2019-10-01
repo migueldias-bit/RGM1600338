725613f7fcadd48b9b4e16f84eea3ae854bc7a97
Gabriel Dias <trimax_08@outlook.com>
2019-10-01 14:09:50 -0300

    Fazendo o commit

---
+++ Banco.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace RECREGM1600338
+{
+    public class Banco
+    {
+        private static string strconn;
+
+        public static MySqlCommand Abrir()
+        {
+            strconn = @"server=127.0.0.1;user id=root;database=atividadeDB_td;password=root"; // String de conexao MySQL WORKBENCH****
+            MySqlConnection cn = new MySqlConnection();
+            cn.Open();
+            MySqlCommand comm = new MySqlCommand();
+            comm.Connection = cn;
+            return comm;
+        }
+
+        public static void Fechar(MySqlCommand comm)
+
+        {
+            var cn = comm.Connection;
+            if(cn.State == ConnectionState.Open)
+            {
+                comm.Connection.Close();
+            }
+    }
+}
+}
